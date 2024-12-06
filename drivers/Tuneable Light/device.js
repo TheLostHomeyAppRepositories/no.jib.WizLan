2f2598a759c64b3bf6eb5d37527b7bfbd52e74cb
Jan-Ivar Bøyum <78592574+boeyum@users.noreply.github.com>
2024-12-06 13:21:08 +0100

    1.1.2
    New version with bug fixes.

--- drivers/Tuneable Light/device.js
+++ drivers/Tuneable Light/device.js
@@ -8,15 +8,10 @@ var ipAddr = null;
 var macAddr = null;
 var devices = null;
 var isState = false;
-var isDimming = true;
-var isTemp = true;
-var isScenes = true;
 
-var stat = false;
 var mydim = 50;
-var scene = null;
+var scene = 0;
 var temp = 0;
-const nums = [0, 6, 11, 12, 13, 14, 15, 16, 18, 30];
 
 class TuneableDevice extends Device {
 
@@ -31,67 +26,53 @@ class TuneableDevice extends Device {
       this.devices = new Command(null);
 
       this.isState = this.devices.getState(this.ipAddr);
-      this.isDimming = true;
-      this.isTemp = true;
-      this.isScenes = true;
-
-      this.stat = this.isState;
 
       this.pollDevice(this.id, this.devices);
 
       this.setCapabilityValue('onoff', this.isState);
       this.registerCapabilityListener('onoff', async (value) => {
-          this.stat = value;
           this.isState = value;
           const settings = this.getSettings();
           this.devices.setOnOff(settings.ip, value);
       });
 
-      if (isDimming) {
-          this.mydim = this.devices.getDimming(this.ipAddr);
-          this.setCapabilityValue('dim', this.mydim);
-          this.registerCapabilityListener('dim', async (value) => {
-              if (value < 0) {
-                  value = 0;
-              } else if (value > 100) {
-                  value = 100;
-              }
-              const settings = this.getSettings();
-              this.devices.setBrightness(settings.ip, value);
-          });
-      }
-
-      if (isTemp) {
-          this.temp = this.devices.getTemperature(this.ipAddr);
-          this.setCapabilityValue('kelvin', this.temp);
-          this.registerCapabilityListener('kelvin', async (value) => {
-              if (value < 2100) {
-                  value = 2100;
-              } else if (value > 6000) {
-                  value = 6000;
-              }
-              const settings = this.getSettings();
-              this.devices.setLightTemp(settings.ip, value);
-          });
-      }
-
-      if (isScenes) {
-          this.scene = this.devices.getScene(this.ipAddr);
-          if (!this.nums.includes(this.scene)) {
-              this.scene = 0;
+      this.mydim = this.devices.getDimming(this.ipAddr);
+      this.setCapabilityValue('dim', this.mydim);
+      this.registerCapabilityListener('dim', async (value) => {
+          if (value < 0) {
+              value = 0;
+          } else if (value > 100) {
+              value = 100;
           }
-          this.setCapabilityValue('scene', this.scene.toString());
-          this.registerCapabilityListener('scene', async (value) => {
-              this.scene = value;
-              const settings = this.getSettings();
-              if (this.scene == 0) {
-                  this.scene = 0;
-                  this.devices.setLightTemp(settings.ip, 2700);
-              } else {
-                  this.devices.setLightScene(settings.ip, this.scene);
-              }
-          });
-      }
+          const settings = this.getSettings();
+          this.devices.setBrightness(settings.ip, value);
+      });
+  
+      this.temp = this.devices.getTemperature(this.ipAddr);
+      this.setCapabilityValue('kelvin', this.temp);
+      this.registerCapabilityListener('kelvin', async (value) => {
+          if (value < 2100) {
+              value = 2100;
+          } else if (value > 6000) {
+              value = 6000;
+          }
+          const settings = this.getSettings();
+          this.devices.setLightTemp(settings.ip, value);
+      });
+  
+      var nums = this.devices.getScene(this.ipAddr);
+      this.scene = this.checkScene(nums);
+      this.setCapabilityValue('scene', this.scene.toString());
+      this.registerCapabilityListener('scene', async (value) => {
+          this.scene = value;
+          const settings = this.getSettings();
+          if (this.scene == 0) {
+              this.scene = 0;
+              this.devices.setLightTemp(settings.ip, 2700);
+          } else {
+              this.devices.setLightScene(settings.ip, this.scene);
+          }
+      });
   }
 
   /**
@@ -140,7 +121,7 @@ class TuneableDevice extends Device {
   }
 
   // HELPER FUNCTIONS
-  pollDevice(id, device) {
+  async pollDevice(id, device) {
       clearInterval(this.pollingInterval);
 
       this.pollingInterval = setInterval(async () => {
@@ -152,6 +133,37 @@ class TuneableDevice extends Device {
           this.setCapabilityValue('kelvin', this.temp);
           this.scene = this.devices.getScene(this.ipAddr);
       }, 600000);
+  }
+
+  checkScene(val) {
+      if (val == 6) {
+          return 6;
+      }
+      if (val == 11) {
+          return 11;
+      }
+      if (val == 12) {
+          return 12;
+      }
+      if (val == 13) {
+          return 13;
+      }
+      if (val == 14) {
+          return 14;
+      }
+      if (val == 15) {
+          return 15;
+      }
+      if (val == 16) {
+          return 16;
+      }
+      if (val == 18) {
+          return 18;
+      }
+      if (val == 30) {
+          return 30;
+      }
+      return 0;
   }
 
   callLightTemp(ktm) {
